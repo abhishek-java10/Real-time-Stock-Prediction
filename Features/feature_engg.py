bd5bea046e7622a74082c9d1da205fe767308d45
Abhishek Teotia <abhishek2486teotia@gmail.com>
2025-04-29 19:34:52 +1000

    feature engg

---
+++ Features/feature_engg.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Loading the dataset and making it a global variable so that every funtion can use it.
+data = pd.read_csv('cleaned_data.csv')
+
+# Reason for taking log of the amount is to normalize the amount.
+def log_amount():
+    pass
+
+# Engineering Volatality feature.
+def volatality():
+    pass
+
+# Valocity Features like()
+# log_amount(data)
+
